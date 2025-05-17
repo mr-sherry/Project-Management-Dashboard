3c9594278de25ee8fe692f357b166cfe1e1c4c87
Ahmed Raza Shahid <mr.sherry6967@gmail.com>
2025-05-17 18:06:05 +0500

    First Commit With Files Structure and Pages and Routes

--- project-managment-dashboard/src/main.jsx
+++ project-managment-dashboard/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Landing from './Pages/Landing/Landing.jsx'
 import Dashboard from './Pages/Dashboard/Dashboard.jsx'
 import Settings from './Pages/Settings/Settings.jsx'
@@ -35,7 +35,9 @@ const routes = createBrowserRouter(
 );
 
 createRoot(document.getElementById('root')).render(
+  <RouterProvider router={routes}>
+    <App />
 
-  <App />
+  </RouterProvider>
 
 )
